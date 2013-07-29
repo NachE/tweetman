967b26a8e9418f97fb5a7b8351b5885741189632
NachE <nache.nache@gmail.com>
2013-07-29 14:00:49 +0200

    deleting old, renaming new

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
 #!/usr/bin/env python
 
-import sys, os
-import getpass
-import tweetmanconfig
-
-# require tweepy with twitter API v1.1 support
-# I use github version: https://github.com/tweepy/tweepy
-import tweepy
-
-print("\nWelcome to tweetman. Copyright by J.A. Nache 2013\n");
-
-#TODO: put it on ~/config/tweetman.conf 
-config = ConfigParser.ConfigParser()
-config.read('tweetman.cfg')
-
-consumer_key = config.get('oauth', 'consumerkey')
-consumer_secret = config.get('oauth', 'consumersecret')
-#TODO: do it automatically:
-access_token = config.get('oauth', 'accesstoken')
-access_token_secret = config.get('oauth', 'accesstokensecret')
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
-#print api.get_user()
-print api.home_timeline()
 
 
+import oauth2 as oauth
+import json
+import tweetmanconfig as config
+
+
+consumer = oauth.Consumer(key=config.consumer_key, secret=config.consumer_secret)
+access_token = oauth.Token(key=config.access_token, secret=config.access_token_secret)
+client = oauth.Client(consumer, access_token)
+
+timeline_endpoint = "http://api.twitter.com/1.1/statuses/home_timeline.json"
+response, data = client.request(timeline_endpoint)
+
+tweets = json.loads(data)
+for tweet in tweets:
+    print tweet['text']
